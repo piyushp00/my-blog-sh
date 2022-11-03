f2f7f8eef01739091a80fee3b7c977c829f3c0a4
Piyush Pandey <piyushpvns@gmail.com>
2022-11-03 10:07:27 +0530

    Add navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ArticleListPage from "./pages/ArticlesListPage";
+import ArticlePage from "./pages/ArticlePage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
-  return <div className="App">
-    <h1>My Awesome blog</h1>
-    <div id="page-body">
-      Welcome to my awesome blog!
-    </div>
-  </div>
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <NavBar />
+        <div id="page-body">
+          <Routes>
+            <Route
+              path="/"
+              element={<HomePage />}
+            />
+            <Route
+              path="/about"
+              element={<AboutPage />}
+            />
+            <Route
+              path="/articles"
+              element={<ArticleListPage />}
+            />
+            <Route
+              path="/article/:articleId"
+              element={<ArticlePage />}
+            />
+            <Route
+              path="*"
+              element={<NotFoundPage />}
+            />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
